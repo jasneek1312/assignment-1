2a934159880deb6d92bf21574686229028832e81
jasneek1312 <130336070+jasneek1312@users.noreply.github.com>
2023-04-10 10:09:34 +0530

    Add files via upload

---
+++ # get three numbers from the user.py
@@ -0,0 +1,10 @@
+# get three numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# calculate the average
+average = (num1 + num2 + num3) / 3
+
+# display the result
+print("The average of", num1, ",", num2, "and", num3, "is", average)
